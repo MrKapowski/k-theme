e9e6e2665f016fb33440e2d8f8682f5566c1cd42
Chris <mrkapowski@gmail.com>
2019-01-04 11:38:26 +0000

    fix display bug caused by adding translation options to searchform.php

--- searchform.php
+++ searchform.php
@@ -2,7 +2,7 @@
 	<div class="input-group mb-3">
 		<input type="search" class="form-control" placeholder="<?php esc_attr_e( 'Search…', 'mrkapowski' ); ?>" aria-label="Search" aria-describedby="button-addon2" name="s">
 		<div class="input-group-append">
-			<button class="btn btn-outline-secondary" type="submit" id="button-addon2"><?php __e( 'Search', 'mrkapowski' ); ?></button>
+			<button class="btn btn-outline-secondary" type="submit" id="button-addon2"><?php esc_html_e( 'Search', 'mrkapowski' ); ?></button>
 		</div>
 	</div>
 </form>
