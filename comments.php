5e9c6c62db45b2a72e1c9a3fbeeb38ae0eed1112
Chris <mrkapowski@gmail.com>
2019-01-04 14:28:00 +0000

    slight reordering

--- comments.php
+++ comments.php
@@ -16,13 +16,12 @@ if ( post_password_required() ) {
 }
 ?>
 <footer>
-	<?php if ( have_comments() ) : ?>
 	<header>
 		<h5 class="comments-title">
 			<?php
 			printf(
 				// translators:
-				esc_html( _n( '%s Comment', '%s Comments', get_comments_number(), 'mrkapowski' ) ),
+				esc_html( _nx( '%s Reply', '%s Replies', get_comments_number(), 'comments title', 'mrkapowski' ) ),
 				esc_html( number_format_i18n( get_comments_number() ) ),
 				'<span>' . esc_html( get_the_title() ) . '</span>'
 			);
@@ -33,19 +32,31 @@ if ( post_password_required() ) {
 		<?php if ( ! comments_open() && get_comments_number() ) : ?>
 		<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'mrkapowski' ); ?></p>
 		<?php endif; ?>
+		<?php if ( have_comments() ) : ?>
 		<ol class="comment-list">
-		<?php
-			wp_list_comments(
-				array(
-					'type'     => 'comment',
-					'callback' => 'mrkapowski_comment',
-				)
-			);
-		?>
+			<?php
+				wp_list_comments(
+					array(
+						'type'     => 'comment',
+						'callback' => 'mrkapowski_comment',
+					)
+				);
+			?>
 		</ol><!-- .comment-list -->
 		<hr>
+		<ol class="comment-list">
+			<?php
+				wp_list_comments(
+					array(
+						'type'     => 'webmention',
+						'callback' => 'mrkapowski_comment',
+					)
+				);
+			?>
+		</ol><!-- .comment-list -->
+		<hr>
+		<?php endif; ?>
 	</section>
-	<?php endif; ?>
 	<footer>
 	<?php if ( comments_open() ) : ?>
 		<div id="commentform-top"></div>
